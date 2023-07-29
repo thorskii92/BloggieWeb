f040b99e3a2e37a018fa28a0a9ffec65971dcfdc
thorskii92 <nestorigna92@gmail.com>
2023-07-29 17:29:07 +0800

    first commit

---
+++ BloggieWeb/Controllers/AdminTagsController.cs
@@ -0,0 +1,39 @@
+﻿using BloggieWeb.Data;
+using BloggieWeb.Models.Domain;
+using BloggieWeb.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BloggieWeb.Controllers
+{
+    public class AdminTagsController : Controller
+    {
+        private readonly BloggieDbContext bloggieDbContext;
+
+        public AdminTagsController(BloggieDbContext bloggieDbContext)
+        {
+            this.bloggieDbContext = bloggieDbContext;
+        }
+
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ActionName("Add")]
+        public IActionResult Add(AddTagRequest addTagRequest)
+        {
+            //Mapping add tag request to the tag domain model
+            var tag = new Tag
+            {
+                Name = addTagRequest.Name,
+                DisplayName = addTagRequest.DisplayName
+            };
+            bloggieDbContext.Tags.Add(tag);
+            bloggieDbContext.SaveChanges();
+            return View("Add");
+        }
+    }
+}
---
+++ BloggieWeb/Data/BloggieDbContext.cs
@@ -0,0 +1,18 @@
+﻿using BloggieWeb.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace BloggieWeb.Data
+{
+    public class BloggieDbContext : DbContext
+    {
+        public BloggieDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<BlogPost> BlogPosts { get; set; }  
+        public DbSet<Tag> Tags { get; set; }
+
+
+    }
+}
